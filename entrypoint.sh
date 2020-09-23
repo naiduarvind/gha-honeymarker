dcddbff678034a02abf5cffcd139232d70df2386
Arvind Naidu <arvind@thebility.engineer>
2020-09-24 02:07:07 +0800

    chore(gha-honeymarker): working bash entrypoint; todo: convert to golang
    Signed-off-by: Arvind Naidu <arvind@thebility.engineer>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+set -euxo pipefail
+
 function install_binary() {
     go get github.com/honeycombio/honeymarker
 }
 
 function create_marker() {
-    honeymarker --writekey=${INPUT_APIKEY} -d ${INPUT_DATASET} add --type=${INPUT_TYPE} -m ${INPUT_MESSAGE}
+    honeymarker -k $INPUT_APIKEY -d $INPUT_DATASET add -t $INPUT_TYPE -m $INPUT_MESSAGE
 }
 
 install_binary
